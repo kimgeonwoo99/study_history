8fa1d63f6fc8cba9c5d42e1e230a6ed9ac393c82
kimgeonwoo99 <ksh56587466@gmail.com>
2024-11-15 14:31:38 +0900

    Add files via upload

---
+++ 프로젝트2/CompanyAddr.py
@@ -0,0 +1,24 @@
+from Addr1 import Addr1
+
+class CompanyAddr(Addr1):
+    def __init__(self, name, phone, email, address, group, birthday, company, department, position):
+        super().__init__(name,phone,email,address,group,birthday,position)
+        self.__company = company
+        self.__department = department
+
+    def get_company(self):
+        return self.__company
+    def set_company(self,value):
+        self.__company = value
+
+    def get_department(self):
+        return self.__department
+    def set_department(self, value):
+        self.__department = value
+
+    def print_info(self):
+        super().print_info()
+        print(f"회사이름 : {self.get_company()}")
+        print(f"부서이름 : {self.get_department()}")
+        print(f"직급 : {self.get_position()}")
+
