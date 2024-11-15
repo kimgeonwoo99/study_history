24109e9fde3f719c679fe24fd7ee25cf9eda7818
kimgeonwoo99 <ksh56587466@gmail.com>
2024-11-15 17:51:59 +0900

    Update Secondrest.py

--- 중국집레스토랑/Secondrest.py
+++ 중국집레스토랑/Secondrest.py
@@ -1,13 +1,13 @@
-from Ownerrest import Ownerrest
+from OwnerRest import OwnerRest
 
-class Secondrest(Ownerrest):
+class SecondRest(OwnerRest):
     def __init__(self,jjajang,jjampong,pork,rice,dumpling=3000):
         super().__init__(jjajang,jjampong,pork,dumpling,rice)
     def print_price(self,idx=2):
         super().print_loc(idx)
-        for key, value in self.price_dict.items():
-            if key == "공기밥":
+        for item, price in self.price_dict.items():
+            if item == "공기밥":
                 print("공기밥 : 무료입니다.")
             else:
-                super().ordin_price(key,value)
+                super().ordin_price(item, price)
         print("="*30)
