5448a3e31dce4d9d29dd63935719efee6cbefdf4
kimgeonwoo99 <ksh56587466@gmail.com>
2024-11-15 17:49:58 +0900

    Update Firstrest.py

--- 중국집레스토랑/Firstrest.py
+++ 중국집레스토랑/Firstrest.py
@@ -1,13 +1,13 @@
-from Ownerrest import Ownerrest
+from OwnerRest import OwnerRest
 
-class Firstrest(Ownerrest):
+class FirstRest(OwnerRest):
     def __init__(self,jjajang,dumpling,jjampong=8000,pork =12000,rice = 1000):
         super().__init__(jjajang,jjampong,pork,dumpling,rice)
     def print_price(self,idx=1):
         super().print_loc(idx)
-        for key, value in self.price_dict.items():
-            if key == "군만두":
+        for item, price in self.price_dict.items():
+            if item == "군만두":
                 print("군만두 : 판매하지 않습니다.")
             else:
-                super().ordin_price(key,value)
+                super().ordin_price(item, price)
         print("="*30)
