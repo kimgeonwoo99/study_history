e829bbebeb54618a2a4789f7bb019ed661de1499
kimgeonwoo99 <ksh56587466@gmail.com>
2024-11-15 17:58:23 +0900

    Update Rest_main.py

--- 중국집레스토랑/Rest_main.py
+++ 중국집레스토랑/Rest_main.py
@@ -4,11 +4,11 @@ from SecondRest import SecondRest
 from ThirdRest import ThirdRest
 
 if __name__ == "__main__":
-    a = Ownerrest(8000, 8000, 12000, 3000, 1000)
+    a = OwnerRest(8000, 8000, 12000, 3000, 1000)
     a.print_price()
-    b = Firstrest(7000, 0)
+    b = FirstRest(7000, 0)
     b.print_price()
-    c = Secondrest(5000, 5000, 10000, 0)
+    c = SecondRest(5000, 5000, 10000, 0)
     c.print_price()
-    d = Thirdrest(7000, 7000)
+    d = ThirdRest(7000, 7000)
     d.print_price()
